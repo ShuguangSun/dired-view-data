80a5f254fc9e0a8f022e429a53c3d8b1da26c4b8
Shuguang Sun <shuguang79@qq.com>
2021-05-10 17:31:09 +0800

    fix commentary.
    Signed-off-by: Shuguang Sun <shuguang79@qq.com>

--- dired-view-data.el
+++ dired-view-data.el
@@ -27,26 +27,17 @@
 ;; View data from dired via ess(-r)
 
 ;; (require 'dired-view-data)
+;; (dired-view-data-global-mode)
+;; or call minor-mode in dired buffer mannualy
+;; (dired-view-data-mode 1)
+;; which make `dired-do-shell-command' (`S-!') recognize those files as well if
+;; `dired-view-data-guess-shell-alist-p' is `t'.
 
-;; In dired buffer, call `dired-view-data` on a data file (e.g., sas7bdat, xpt,
-;; rds, cs, rda or rdata), and buffer will pop up with data displayed.
+;; In dired buffer, call `dired-view-data` (`V' or `C-c C-v') on a data file
+;; (e.g., sas7bdat, xpt, rds, csv, rda or rdata), and buffer will pop up with
+;; data displayed.
 
-;; Call `dired-view-data-initialization` can do some set up. It define key `C-c
-;; C-r` for dired-view-data in dired-mode, and
-
-;; (add-to-list 'dired-guess-shell-alist-user
-;;              (list "\\.\\(sas7bdat\\|xpt\\|rds\\|csv\\|rda\\|rdata\\)$"
-;;                    '(progn
-;;                       (if (y-or-n-p-with-timeout "Read to R? " 4 nil)
-;;                           (progn
-;;                             (dired-view-data--do (dired-get-filename))
-;;                             (keyboard-quit))
-;;                         (if (eq system-type 'windows-nt)  ;; for w32
-;;                             (w32-shell-execute "open" file-name nil 1))))))
-
-;; which make `dired-do-shell-command` (`S-!`) for those files.
-
-;; You can modify or add new format via `dired-view-data-data-name-format`.
+;; You can modify or add new format via `dired-view-data-data-name-format'.
 
 ;;; Code:
 
